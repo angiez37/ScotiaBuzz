481d4d7951d0ba4b2be30b8f51adc92d2213dc48
Taha Zaryab <tahazaryab10@gmail.com>
2024-08-10 13:07:14 -0400

    create detector page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,11 +1,33 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
 
 const Home = (): JSX.Element => {
-    const navigate = useNavigate()
+    //const navigate = useNavigate()
+
     return (
-        <div className="min-h-screen bg-gray-50 flex flex-col justify-center">
-            <p>test</p>
-        </div>
+        <>
+            <NavBar />
+            <div className="bg-white flex flex-col relative z-20 text-center items-center overflow-hidden ">
+                <div className="container mx-auto px-6 flex flex-col items-center text-center relative py-12">
+                    <h1 className="font-bebas-neue text-2xl uppercase font-black flex flex-col leading-none text-gray-800">
+                        Scotia Fraud Risk Detector
+                    </h1>
+                    <p className="text-sm sm:text-base text-black pt-10 max-w-md">
+                        Upload Content
+                    </p>
+                    
+                </div>
+
+                <textarea name="postContent" rows={10} cols={40} className="w-[700px] border bg-gray-50 rounded-md" />
+                <button onClick={() => alert("clicked")} className="bg-transparent my-6 hover:bg-[#ff0000] text-black font-semibold hover:text-white py-2 px-4 border border-[#ff0000] hover:border-transparent rounded">
+                        Submit
+                    </button>
+            </div>
+
+
+
+
+        </>
     );
 };
 
---
+++ src/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navbar = () => {
+    const navItems = [
+        { id: 1, text: 'Home' },
+        { id: 2, text: 'Fraud Detector' },
+        { id: 3, text: 'Placeholder' },
+    ];
+
+    return (
+        <div className='flex justify-between items-center h-24 mx-auto px-4 text-black bg-gray-50'>
+            {/* Logo */}
+            <img src="scotia-logo.webp" className='w-[300px]' alt='Scotiabank Logo' />
+
+            <ul className='hidden md:flex'>
+                {navItems.map(item => (
+                    <li
+                        key={item.id}
+                        className='p-4 hover:bg-[#ff0000] rounded-xl m-4 cursor-pointer duration-300 hover:text-white'
+                    >
+                        {item.text}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
